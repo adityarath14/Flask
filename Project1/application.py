6bdd9b85eab01d77d84af89de6859a53fd95129c
adityarath14 <adityarath14042001@gmail.com>
2024-11-29 17:53:41 +0530

    Flask Project For Static Files

--- Project1/application.py
+++ Project1/application.py
@@ -6,5 +6,11 @@ def First():
 @FAI.route('/aditya')
 def aditya():
     return render_template('aditya.html')
+@FAI.route('/demo/<data>/')
+def demo(data):
+    return f'demo {data}'
+@FAI.route('/view')
+def view():
+    return render_template('view.html')
 if __name__=='__main__':
-    FAI.run(debug=True)
+    FAI.run(debug=True,host='192.168.43.95',port=5001)
